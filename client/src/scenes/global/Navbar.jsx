9faa81155596081a23c08114b3396185c808b260
NigelCMD <nigel.m.d98@gmail.com>
2023-11-10 11:50:41 -0500

    added nav bar and started shopping cart slider

--- client/src/scenes/global/Navbar.jsx
+++ client/src/scenes/global/Navbar.jsx
@@ -1,7 +1,7 @@
 import {useDispatch, useSelector} from 'react-redux'
 import {Badge, Box, IconButton} from "@mui/material"
 import {
-    PersonOutline,
+    PersonOutlined,
     ShoppingBagOutlined,
     MenuOutlined,
     SearchOutlined
@@ -9,10 +9,14 @@ import {
 
 import {useNavigate, useNavigationType } from 'react-router-dom';
 import {shades} from '../../theme'
+import { setIsCartOpen } from '../../state';
+
 
 const Navbar = ()=>{
 const navigate = useNavigate();
 const dispatch = useDispatch();
+const cart = useSelector((state) => state.cart.cart)
+
 
     return(
         <Box 
@@ -55,9 +59,29 @@ const dispatch = useDispatch();
             <IconButton sx={{color:'black'}}>
                 <PersonOutlined/>
             </IconButton>
-            <IconButton sx={{color:'black'}}>
+            <Badge
+                badgeContent={cart.length}
+                color='secondary'
+                invisible={cart.length === 0}
+                sx={{
+                    '& .MuiBadge-badge': {
+                        right: 5,
+                        top: 5,
+                        padding: '0 4px',
+                        height: '14px',
+                        minWidth: '13px'
+                    },
+                }}
+            >
+
+            <IconButton 
+                onClick={() => dispatch(setIsCartOpen({}))}
+                sx={{color:'black'}}
+            >
                 <ShoppingBagOutlined/>
             </IconButton>
+            
+            </Badge>
             <IconButton sx={{color:'black'}}>
                 <MenuOutlined/>
             </IconButton>
